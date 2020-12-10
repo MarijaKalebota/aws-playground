d64d6283118a9faa8ea069704dad96991d704f3b
Marija Kalebota Kodzoman <marija.kalebota@gmail.com>
2020-12-10 15:36:58 -0800

    Add practice with dotenv

--- list_s3_buckets.py
+++ list_s3_buckets.py
@@ -1,6 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 import boto3
 
-s3 = boto3.resource('s3')
+#s3 = boto3.resource('s3')
+s3 = boto3.resource('s3', aws_access_key_id=os.environ.get("AWS_KEY_ID"),
+                      aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"))
 
 for bucket in s3.buckets.all():
     print(bucket.name)
